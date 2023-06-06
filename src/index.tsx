9dcb34fe3a56b80e95136ff7d8fb2c636ced2aa5
Ilja Laurs <iljal@peplink.com>
2023-06-06 14:57:04 +0300

    test of cluster imp

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,32 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+const cluster = require("cluster");
+const os = require("os");
+const numCPUs = os.cpus().length;
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // Listen for dying workers and create new ones
+  cluster.on("exit", (worker: any) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
